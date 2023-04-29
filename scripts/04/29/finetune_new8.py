7f47e02e1a9e5fc67edfe72c26dc17e158729f55
Roman Malashin <Roman Malashin>
2023-04-29 18:33:40 +0300

    upd plane

--- scripts/04/29/finetune_new8.py
+++ scripts/04/29/finetune_new8.py
@@ -7,7 +7,7 @@ import random
 import sys
 sys.path.append(".")
 from script_manager.func.script_boilerplate import do_everything
-from local_config import IR_VIS_DATASET_PATH_NEW as IR_VIS_DATASET_PATH
+from local_config import IR_VIS_DATASET_PATH_NEW2 as IR_VIS_DATASET_PATH
 
 # weights and biases project name
 WANDB_PROJECT_NAME = "python-image-models"
@@ -20,7 +20,6 @@ extra_folder_keys = []
 # used for 1000 classes
 default_parameters = {
     "__script_output_arg__": "output",
-    "batch-size": 64,
     "sched": "step",
     "epochs": 200,
     "workers": 24,
@@ -32,7 +31,7 @@ default_parameters = {
     "drop-path": 0.2,
     # "model-ema":  "parameter_without_value",
     # "model-ema-decay": 0.9999,
-    "aa": "rand-m9-mstd0.5",
+    # "aa": "rand-m9-mstd0.5",
     "remode": "pixel",
     "reprob": 0.2,
     "amp": "parameter_without_value",
@@ -41,6 +40,7 @@ default_parameters = {
     "data-dir": os.path.join(IR_VIS_DATASET_PATH, 'pytorch_models_structure', 'day'),
     "hflip": 0,
     "disable_geometry_aug": "parameter_without_value",
+    "color-jitter": 0,
 }
 
 configs = []
@@ -51,39 +51,19 @@ test_parameters = {
 
 MAIN_SCRIPT = f"torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:{random.randint(0,1000)} --nproc_per_node=1 train.py"
 
-# for lr in [1e-1,1e-2,1e-3,1e-4]:
-config = {
-    "aa": "pigeon",
-    "batch-size": 64,
-    "lr": 1e-1,
-    "decay-rate": 0.1,
-    "decay-epochs": 80,
-    "model": 'resnet18',
-    "tag": f"{base_tag}_day_tune",
-}
-configs.append([config, None])
 
 config = {
-    "__script_output_arg__": "output",
-    "amp": "parameter_without_value",
-    "batch-size": 64, #-b 512 
-    "decay-epochs": 2.4,
-    "decay-rate": 0.973,
-    "drop": 0.2,
-    "drop-path": 0.2,
-    "epochs": 600,
-    "lr": 0.064,
-    "lr-noise": "0.42 0.9",
-    "model-ema":  "parameter_without_value",
-    "model-ema-decay": 0.9999,
-    "opt": "rmsproptf",
-    "opt-eps": 0.001,
-    "sched": "step", #--sched step 
-    "warmup-lr": 1e-6,
-    "weight-decay": 1e-5,
-    "workers": 24,
-    "aa": "pigeon",
-    "tag": f"{base_tag}_rmsprop"
+    "lr": 1e-1,
+    "batch-size": 64,
+    "decay-rate": 0.1,
+    "decay-epochs": 80,
+    "model": "efficientnet_b6",
+    "random_invert_p": 0.5,
+    "adjust_sharpness": 10,
+    "color-jitter": 0.4,
+    "aa": "rand-m9-mstd0.5",
+    "to_grayscale": "parameter_without_value",
+    "tag": f"{base_tag}_aa_inv_prob_0.5_rand_sharpness_color_jitter_grayscale_resnet50",
 }
 configs.append([config, None])
 
