e7020f4590171b463d9ac77300c67cb114a2e1d7
Roman Malashin <malashinroman@mail.ru>
2023-04-29 10:42:37 +0300

    pegion

--- scripts/04/29/experiment_pegion.py
+++ scripts/04/29/experiment_pegion.py
@@ -52,16 +52,40 @@ test_parameters = {
 MAIN_SCRIPT = f"torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:{random.randint(0,1000)} --nproc_per_node=1 train.py"
 
 # for lr in [1e-1,1e-2,1e-3,1e-4]:
-for batch_size in [64]:
-    config = {
-        "aa": "pigeon",
-        "lr": 1e-1,
-        "decay-rate": 0.1,
-        "decay-epochs": 80,
-        "model": 'resnet18',
-        "tag": f"{base_tag}_day_tune_{batch_size}",
-    }
-    configs.append([config, None])
+config = {
+    "aa": "pigeon",
+    "batch-size": 64,
+    "lr": 1e-1,
+    "decay-rate": 0.1,
+    "decay-epochs": 80,
+    "model": 'resnet18',
+    "tag": f"{base_tag}_day_tune_{batch_size}",
+}
+configs.append([config, None])
+
+config = {
+    "__script_output_arg__": "output",
+    "amp": "parameter_without_value",
+    "batch-size": 64, #-b 512 
+    "decay-epochs": 2.4,
+    "decay-rate": 0.973,
+    "drop": 0.2,
+    "drop-path": 0.2,
+    "epochs": 600,
+    "lr": 0.064,
+    "lr-noise": "0.42 0.9",
+    "model-ema":  "parameter_without_value",
+    "model-ema-decay": 0.9999,
+    "opt": "rmsproptf",
+    "opt-eps": 0.001,
+    "sched": "step", #--sched step 
+    "warmup-lr": 1e-6,
+    "weight-decay": 1e-5,
+    "workers": 24,
+    "aa": "pigeon",
+    "tag": f"{base_tag}_rmsprop"
+}
+configs.append([config, None])
 
 # RUN everything
 # !normally you don't have to change anything here
