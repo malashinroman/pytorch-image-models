1e9ee9484e4aee77cd208235ae0622ba022893b7
Roman Malashin <Roman Malashin>
2023-05-01 03:11:28 +0300

    run

---
+++ scripts/04/30/fix_lr_noise.py
@@ -0,0 +1,118 @@
+"""
+train series of mobilenets on 100 subclasses of imagenet
+"""
+
+import os
+import random
+import sys
+sys.path.append(".")
+from script_manager.func.script_boilerplate import do_everything
+from local_config import IR_VIS_DATASET_PATH_NEW2 as IR_VIS_DATASET_PATH
+
+# weights and biases project name
+WANDB_PROJECT_NAME = "python-image-models"
+base_tag = os.path.split(__file__)[-1].split('.')[0]
+# keys
+appendix_keys = ["tag"]
+extra_folder_keys = []
+
+default_parameters = {
+    "__script_output_arg__": "output",
+    # "aa": "rand-m9-mstd0.5",
+    "amp": "parameter_without_value",
+    "batch-size": 64, #-b 512 
+    "decay-epochs": 2.4,
+    "decay-rate": 0.973,
+    "drop": 0.2,
+    "drop-path": 0.2,
+    "epochs": 600,
+    "lr": 0.064,
+    "lr-noise": "0.42 0.9",
+    "model-ema":  "parameter_without_value",
+    "model-ema-decay": 0.9999,
+    "opt": "rmsproptf",
+    "opt-eps": 0.001,
+    "remode": "pixel",
+    "reprob": 0.2,
+    "sched": "step", #--sched step 
+    "warmup-lr": 1e-6,
+    "weight-decay": 1e-5,
+    "workers": 24,
+    "test-split": "test",
+    "disable_geometry_aug": "parameter_without_value",
+    "model": 'resnet18',
+    "data-dir": os.path.join(IR_VIS_DATASET_PATH, 'pytorch_models_structure', 'day'),
+    "hflip": 0,
+}
+
+configs = []
+
+test_parameters = {
+
+}
+
+MAIN_SCRIPT = f"torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:{random.randint(0,1000)} --nproc_per_node=1 train.py"
+
+for color_jitter in [0.4]:
+    config = {
+        "color-jitter": color_jitter,
+        "tag": f"{base_tag}_day_color_jitter_{color_jitter}",
+    }
+    configs.append([config, None])
+
+for color_jitter in [0.4]:
+    config = {
+        "tag": f"{base_tag}_day_color_jitter_{color_jitter}_random_invert_p_0.5",
+        "random_invert_p": 0.5,
+        "color-jitter": color_jitter
+    }
+    configs.append([config, None])
+
+for color_jitter in [0.4]:
+    config = {
+        "tag": f"{base_tag}_day_random_invert_p_0.5_grayscale_aa_",
+        "to_grayscale": "parameter_without_value",
+        "random_invert_p": 0.5,
+        "aa": "rand-m9-mstd0.5",
+    }
+    configs.append([config, None])
+
+
+config = {
+    "tag": f"{base_tag}_day_aa_",
+    # "to_grayscale": "parameter_without_value",
+    # "random_invert_p": 0.5,
+    "aa": "rand-m9-mstd0.5",
+}
+configs.append([config, None])
+
+# for color_jitter in [0.2, 0.6, 0.8]:
+#     config = {
+#         "tag": f"{base_tag}_day_color_jitter_{color_jitter}_random_invert_p_0.5",
+#         "to_grayscale": "parameter_without_value",
+#         "random_invert_p": 0.5,
+#         "color-jitter": color_jitter
+#     }
+#     configs.append([config, None])
+#
+# for color_jitter in [0.2, 0.6, 0.8]:
+#     config = {
+#         "tag": f"{base_tag}_day_color_jitter_{color_jitter}",
+#         "to_grayscale": "parameter_without_value",
+#         "color-jitter": color_jitter
+#     }
+#     configs.append([config, None])
+#
+# RUN everything
+# !normally you don't have to change anything here
+if __name__ == "__main__":
+    do_everything(
+        default_parameters=default_parameters,
+        configs=configs,
+        extra_folder_keys=extra_folder_keys,
+        appendix_keys=appendix_keys,
+        main_script=MAIN_SCRIPT,
+        test_parameters=test_parameters,
+        wandb_project_name=WANDB_PROJECT_NAME,
+        script_file=__file__,
+    )
