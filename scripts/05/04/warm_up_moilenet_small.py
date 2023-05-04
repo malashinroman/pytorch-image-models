c3f31c0903931aec582141e2210a12bf4a999eeb
Roman Malashin <Roman Malashin>
2023-05-04 20:13:17 +0300

    upd

--- scripts/05/04/warm_up_moilenet_small.py
+++ scripts/05/04/warm_up_moilenet_small.py
@@ -7,7 +7,7 @@ import random
 import sys
 sys.path.append(".")
 from script_manager.func.script_boilerplate import do_everything
-from local_config import IR_VIS_DATASET_PATH_NEW2 as IR_VIS_DATASET_PATH
+from local_config import IMAGENET_PATH as IMAGENET_PATH
 
 # weights and biases project name
 WANDB_PROJECT_NAME = "python-image-models"
@@ -16,37 +16,34 @@ base_tag = os.path.split(__file__)[-1].split('.')[0]
 appendix_keys = ["tag"]
 extra_folder_keys = []
 
-
 # same parameters that are
 # used for 1000 classes
 default_parameters = {
     "__script_output_arg__": "output",
+    "model": "mobilenetv3_small_100",
+    "batch-size": 512,
     "sched": "step",
     "epochs": 200,
     "workers": 24,
     'warmup-epochs': 0,
-    "opt": "sgd",
+    "decay-rate": 0.2,
+    "decay-epochs": 80,
+    "opt": "rmsproptf",
+    "opt-eps": 0.001,
     "warmup-lr": 1e-6,
     "weight-decay": 1e-5,
     "drop": 0.2,
     "drop-path": 0.2,
-    "batch-size": 64,
-    "decay-rate": 0.1,
-    "decay-epochs": 80,
-    "lr": 1e-1,
     # "model-ema":  "parameter_without_value",
     # "model-ema-decay": 0.9999,
-    # "aa": "rand-m9-mstd0.5",
-    "model": "resnet18",
+    "aa": "rand-m9-mstd0.5",
     "remode": "pixel",
     "reprob": 0.2,
     "amp": "parameter_without_value",
+    "lr": 1e-4,
     "pretrained": "parameter_without_value",
-    "test-split": "test",
-    "data-dir": os.path.join(IR_VIS_DATASET_PATH, 'pytorch_models_structure', 'day'),
-    "hflip": 0.,
-    "disable_geometry_aug": "parameter_without_value",
-    "color-jitter": 0,
+    # "lr-noise": "0.42 0.9",
+    "data-dir": os.path.join(IMAGENET_PATH, 'pytorch_models_structure'),
 }
 
 configs = []
@@ -57,39 +54,15 @@ test_parameters = {
 
 MAIN_SCRIPT = f"torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:{random.randint(0,1000)} --nproc_per_node=1 train.py"
 
-
-# for color_jitter in [0.4]:
-#     config = {
-#         "adjust_sharpness": 10,
-#         "color-jitter": color_jitter,
-#         "force-color-jitter": "parameter_without_value",
-#         "random_invert_p": 0.5,
-#     }
-#     configs.append([config, None])
-
-for color_jitter in [0.4]:
+for i  in range(5):
     config = {
-        "aa": "rand-m9-mstd0.5",
-        "adjust_sharpness": 10,
-        "color-jitter": color_jitter,
-        "force_color_jitter": "parameter_without_value",
-        "random_invert_p": 0.5,
-        "tag": f"{base_tag}_day_FORCE_color_jitter_{color_jitter}_aa_random_invert_p_0.5_sharp",
+        'warmup-epochs': 5,
+        "class-map": f"class_maps/class_maps_five_in_place/class_map{i}.txt",
+        "tag": f"{base_tag}_class_map_{i}_warm_5",
     }
     configs.append([config, None])
 
-
-for color_jitter in [0.4]:
-    config = {
-        # "aa": "rand-m9-mstd0.5",
-        "adjust_sharpness": 10,
-        "color-jitter": color_jitter,
-        "force_color_jitter": "parameter_without_value",
-        "random_invert_p": 0.5,
-        "tag": f"{base_tag}_day_FORCE_color_jitter_{color_jitter}_NO_aa_random_invert_p_0.5_sharp",
-    }
-    configs.append([config, None])
-
+# RUN everything
 # !normally you don't have to change anything here
 if __name__ == "__main__":
     do_everything(
